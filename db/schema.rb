fbb1e50c33bc2ac09c73602b733d4fd7ee8f1b5e
uratomo <rop.ibuki@gmail.com>
2020-06-21 00:23:06 +0900

    apiサーバーサンプル

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_20_130724) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "product"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
